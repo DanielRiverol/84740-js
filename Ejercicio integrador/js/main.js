55c15de2fa57c7f5ea51af7031db59e9acd22f35
Daniel Riverol <dannyriverol@hotmail.com>
2025-12-01 18:48:50 -0300

    Add initial HTML structure and JavaScript for ATM functionality
    - Created index.html with basic layout and structure for the ATM application.
    - Added main.js with comments outlining the ATM operations and user interaction flow.

---
+++ Ejercicio integrador/js/main.js
@@ -0,0 +1,14 @@
+//💰El cajero automático
+//Debemos generar un script para la operación de un cajero automático
+/* El mismo tiene que: 
+1- Mostrar un mensaje de bienvenida
+2- Permitir hasta 3 intentos de ingreso con el PIN del cliente
+  2a- Si ingresa correctamente dar mensaje de Bienvenida
+  2b- Terminados los intentos informar al cliente que se comunique con el banco
+3- Una vez ingresado mostrar las siguientes opciones en pantalla: 
+  a- Saldo 💵
+  b- Retiro 💸
+  c- Depósito 💰
+  d- Salir ❌
+4- Al finalizar mostrar mensaje
+*/
