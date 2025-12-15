c016a555254735f74ebbceaefee711c0bd372899
Daniel Riverol <dannyriverol@hotmail.com>
2025-12-15 14:39:44 -0300

    Add initial HTML structure and JavaScript file for Clase 5 on Arrays and Objects

---
+++ clase_4/js/main.js
@@ -0,0 +1,32 @@
+// arrays
+let arrayVacio = [];
+console.log(arrayVacio);
+let numeros = [10, 31, -2, 3, 40004, 5, 6, 7, 8];
+console.log(numeros);
+
+const cajonCubiertos = ["tenedores", "cucharas", "cuchillos"];
+// console.log(cajonCubiertos);
+// console.log(cajonCubiertos[0]);
+// console.log(cajonCubiertos[1]);
+// console.log(cajonCubiertos[2]);
+
+
+// asiganr valores
+cajonCubiertos[4] = "cucharitas";
+
+// console.log(cajonCubiertos);
+// // cajonCubiertos = false
+// console.log(cajonCubiertos);
+
+// const saludar = () => {
+//   console.log("hola");
+// };
+// saludar();
+
+// saludar = 1;
+// console.log(saludar());
+
+for (let i = 0; i < cajonCubiertos.length ; i++) {
+  console.log(cajonCubiertos[i]);
+}
+// console.log(cajonCubiertos.length);
