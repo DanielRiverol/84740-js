da79e8bd828def565e3a83933cb8319756fd1b94
Daniel Riverol <dannyriverol@hotmail.com>
2026-01-12 22:34:58 -0300

    Eventos

--- clase_7b/js/main.js
+++ clase_7b/js/main.js
@@ -31,3 +31,56 @@ const servicios = [
     img: "micropigmentacion.jpeg",
   },
 ];
+
+const caja = document.getElementById('caja')
+
+
+// caja.addEventListener('click',()=>{
+//   console.log('Hiciste clic');
+//   caja.style.backgroundImage = "url(./img/pum.png)";
+// })
+// caja.addEventListener("mouseenter",()=>{
+//   console.log('Entraste a la caja');
+//   caja.style.backgroundImage= "url(./img/estados.jpg)"
+  
+// })
+// caja.addEventListener("mouseleave",()=>{
+//   console.log('Salist a la caja');
+//    caja.style.backgroundImage = "url(./img/australia.jpg)";
+  
+// })
+
+const inputSearch = document.getElementsByTagName("input")[0];
+const contenedor = document.getElementById("contenedor")
+// console.log(inputSearch);
+function crearHtml(el) {
+  //llamo al elemento contenedor
+  contenedor.innerHTML = "";
+  //creo una variable con un estructura html
+  let html = `<div class="card">
+                <img src=" ./img/${el.img}" alt="${el.nombre}">
+                <hr>
+                <h3>${el.nombre}</h3>
+                <p>Precio: $${el.precio} </p>
+                  <div class="card-action">
+                    <button class="btn" id="${el.id}">Comprar</button>
+                  </div>
+              </div>`;
+  //se la agrego al contenedor
+
+  //modificar para que muestre un cartel producto no encontrado
+  contenedor.innerHTML = html;
+}
+function buscarServicio(arr, filtro) {
+  const encontrado = arr.find((el) => {
+    return el.nombre.includes(filtro);
+  });
+  return encontrado;
+}
+
+
+//  
+const encontrado = buscarServicio(servicios, prompt())
+console.log(encontrado);
+inputSearch.value= "Manzana"
+ crearHtml(encontrado)
