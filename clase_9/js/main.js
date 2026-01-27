393caae8835e2cda8ca5d46b0822680698a140f0
Daniel Riverol <dannyriverol@hotmail.com>
2026-01-26 21:46:04 -0300

    feat: add product entry form and display functionality
    - Created index.html for product entry with form fields for name, description, price, size, and image URL.
    - Implemented main.js to handle product storage and dynamic HTML generation for displaying products.
    - Added materialize.js to initialize select elements for size selection.

--- clase_9/js/main.js
+++ clase_9/js/main.js
@@ -24,4 +24,119 @@ const servicios = [
   },
 ];
 
+btn.addEventListener("click", () => {
+  setTimeout(() => {
+    card.classList.remove("active");
+  }, 2500);
+  card.classList.add("active");
 
+  // card.style.display='block'
+});
+
+// PROMESAS
+
+// Evento futuro
+// console.log('Hola tarola');
+// console.log(
+//   new Promise((resolve, reject) => {
+//     // cuerpo de la promessa
+//   }),
+// );
+// console.log('Hola tarola');
+
+const eventoFuturo = (response) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (response === true) {
+        resolve("Promesa resuelta");
+      } else {
+        reject("Promesa rechazada");
+      }
+    }, 2000);
+  });
+};
+
+// const resultadoExitoso= eventoFuturo(true).then(res=>{
+//   console.log(res);
+
+// })
+// console.log(resultadoExitoso);
+// const resultadoRechazado= eventoFuturo(false).catch(error=>{
+//   console.log(error);
+
+// })
+//  console.log(resultadoRechazado);
+
+// const resultado = eventoFuturo(false)
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const devolverCD = (respuesta) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (respuesta === 5) {
+        resolve("Si tomá gracias por prestarlo");
+      } else {
+        reject("Uh lo perdí");
+      }
+    }, 3000);
+  });
+};
+
+console.log("Te acordas el cd que te presté?");
+devolverCD(5)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+console.log("Sigo haciendo cosas");
+
+const renderServicios = (arr) => {
+  let html;
+  for (const item of arr) {
+    const { id, nombre, img, precio } = item;
+    html = `
+        <div class="card">
+      		<div class="card-image">
+        	<img src="../img/${img}">
+        	<span class="card-title">${nombre.toUpperCase()}</span>
+       </div>
+     	 <div class="card-content"> 
+     		<p>$${precio}</p>
+      </div>
+      <div class="card-action">
+        <button class="btn btn-normal" id="${id}">Comprar</button>
+      </div>
+     </div>
+     `;
+    contenedor.innerHTML += html;
+  }
+};
+
+const pedirServicios = (arr) => {
+  // contenedor.innerHTML = "CARGANDO...⏱️ ";
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (arr) {
+        resolve(arr);
+      } else {
+        reject("Error al cargar los datos");
+      }
+    }, 3000);
+  });
+};
+pedirServicios(false)
+  .then((res) => {
+    renderServicios(res);
+  })
+  .catch((error) => {
+    contenedor.innerHTML = error;
+  });
